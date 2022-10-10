4aaa9e1c0ec9380411e3642bb240644fe2f65b1b
Supriya Kumari <56879125+12345supu@users.noreply.github.com>
2022-10-10 23:57:48 +0530

    Create check kth bit is set or not.cpp

---
+++ Bit Magic/check kth bit is set or not.cpp
@@ -0,0 +1,35 @@
+Method 1 (Left Shift)
+  
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+    if (n & (1 << (k - 1)) != 0)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+    return 0;
+}
+
+.............................................................
+  
+Method 2 (Right Shift)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+    if (((n >> (k - 1)) & 1) != 0)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+    return 0;
+}
+
+
