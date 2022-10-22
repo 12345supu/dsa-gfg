9f94693cad879b973d8aa65f44932730f7961577
Supriya Kumari <56879125+12345supu@users.noreply.github.com>
2022-10-22 10:13:15 +0530

    Create rope cutting prolem.cpp

---
+++ Recursion/rope cutting prolem.cpp
@@ -0,0 +1,12 @@
+int maxPiece(int n, int a, int b, int c)
+{
+    if(n == 0)
+        return 0;
+    if(n < 0)
+        return -1;
+    int res = max((maxPiece(n-a, a, b, c)), (maxPiece(n-b, a, b, c)));
+    res=max(res,(maxPiece(n-c, a, b, c)));
+    if(res == -1)
+        return -1;
+    return res+1;
+}
